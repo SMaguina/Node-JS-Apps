6aed8b073d996a78a16237c22db880b5f123b514
Sylvia Maguiña <SMag23@gmail.com>
2016-07-06 21:24:52 -0700

    Comments added

--- Notes/app.js
+++ Notes/app.js
@@ -1,11 +1,14 @@
+//Create function for process.argv property 
 function grab(flag) {
 	var index = process.argv.indexOf(flag);
 	return (index === -1) ? null : process.argv[index+1];
 }
 
+//Create variables to grab user and greeting values in array
 var greeting = grab('--greeting');
 var user = grab('--user');
 
+//Conditional statement based on user and greeting values provided. Values are places in template strings
 if (!user || !greeting) {
 	console.log("You Blew it!");
 } else {
